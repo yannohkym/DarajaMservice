36b9271b86e08db54c492613eb3060aeb461b27d
Yannohkym <iankym1997@gmail.com>
2024-07-05 01:33:43 +0300

    mservice

---
+++ DataBaseHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpesaService
+{
+  
+  
+
+    public class DataBaseHelper
+    {
+        private string sourceConnectionString;
+        private string destinationConnectionString;
+
+        public DataBaseHelper(string sourceConnectionString, string destinationConnectionString)
+        {
+            this.sourceConnectionString = sourceConnectionString;
+            this.destinationConnectionString = destinationConnectionString;
+        }
+
+        public void TransferUniqueRecords(string sourceTable, string destinationTable)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection sourceConnection = new SqlConnection(sourceConnectionString))
+            {
+                sourceConnection.Open();
+
+                // Read records from source table
+                using (SqlCommand readCommand = new SqlCommand($"SELECT * FROM {sourceTable}", sourceConnection))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(readCommand))
+                {
+                    adapter.Fill(dataTable);
+                }
+            }
+
+            using (SqlConnection destinationConnection = new SqlConnection(destinationConnectionString))
+            {
+                destinationConnection.Open();
+
+                // Insert unique records into destination table
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    if (!RecordExists(destinationConnection, destinationTable, row))
+                    {
+                        string columns = string.Join(", ", dataTable.Columns.Cast<DataColumn>().Select(col => col.ColumnName));
+                        string values = string.Join(", ", dataTable.Columns.Cast<DataColumn>().Select(col => "@" + col.ColumnName));
+
+                        string insertCommandText = $"INSERT INTO {destinationTable} ({columns}) VALUES ({values})";
+
+                        using (SqlCommand insertCommand = new SqlCommand(insertCommandText, destinationConnection))
+                        {
+                            foreach (DataColumn column in dataTable.Columns)
+                            {
+                                insertCommand.Parameters.AddWithValue("@" + column.ColumnName, row[column]);
+                            }
+
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool RecordExists(SqlConnection connection, string destinationTable, DataRow row)
+        {
+            // Assuming Id is the unique key for simplicity
+            int id = (int)row["Id"];
+            string checkCommandText = $"SELECT COUNT(1) FROM {destinationTable} WHERE Id = @Id";
+
+            using (SqlCommand checkCommand = new SqlCommand(checkCommandText, connection))
+            {
+                checkCommand.Parameters.AddWithValue("@Id", id);
+                return (int)checkCommand.ExecuteScalar() > 0;
+            }
+        }
+    }
+
+
+
+}
+
---
+++ Service1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using static MpesaService.DataBaseHelper;
+
+namespace MpesaService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        Timer timer = new Timer();
+
+
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Transfer data Service is started at " + DateTime.Now);
+            TransferData();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds
+            timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+            TransferData();
+            WriteToFile("Transfer data Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Transfer data Service is recalled at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+
+        public void TransferData()
+        {
+            string sourceConnectionString = "Server=YANNOH; Database=ProductApi;User ID=sa;Password=Pass@123; TrustServerCertificate=true;";
+            string destinationConnectionString = "Server=YANNOH; Database=Umoja_RubberHO;User ID=sa;Password=Pass@123; TrustServerCertificate=true;";
+            string sourceTable = "SourceTable";
+            string destinationTable = "DestinationTable";
+
+            DataBaseHelper dbHelper = new DataBaseHelper(sourceConnectionString, destinationConnectionString);
+            dbHelper.TransferUniqueRecords(sourceTable, destinationTable);
+
+            Console.WriteLine("Unique records transferred successfully.");
+        }
+    }
+}
